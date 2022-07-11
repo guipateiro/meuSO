573c66052ac4bd4043e7cedb8f8af5b7ab7e00df
guipateiro <guilhermepateiro@gmail.com>
2022-07-11 17:59:05 -0300

    trabalho t4 terminado e comentado

--- t4/ppos_core.c
+++ t4/ppos_core.c
@@ -1,3 +1,5 @@
+// GRR20197152 GUILHERME COSTA PATEIRO
+// Data da ultima modificacao 11/07/2022 17:58 
 
 #include "ppos_data.h"
 #include "ppos.h"
@@ -6,33 +8,40 @@
 #include <stdlib.h>
 #include <ucontext.h>
 
+
+//estados do programa
 #define TERMINADA 0
 #define RODANDO 3
 #define DORMINDO 2
 #define PRONTA 1
-task_t *MAIN;
-task_t *MAINAUX;
-task_t *DISPATCHER;
-task_t *ATUAL;
-int ID = 0;
-int TAMBUFFER = 65536; //1024* 64
-int CODE = 1;
 
-task_t *FILA_PRONTOS;
+//variaveis globais do core
+task_t *MAIN;			//gurada a task main
+task_t *MAINAUX;		//guarda o a task que deve ser retornada pelo programas criados (atualmente o dispatcher)
+task_t *DISPATCHER;		//guarda a task dispatcher	
+task_t *ATUAL;			//guarda a task que esta atualemtne em execusao
+int ID = 0;				// id que serao atribuidos as novas tasks
+int TAMBUFFER = 65536; 	//1024* 64 memoria alocada para cada contexto
+int CODE = 1;			//codigo de retorno da ultima funcao
 
+task_t *FILA_PRONTOS;   //fila de task que estao prontas para serem executada
+
+//funcao qu selecioana a proxima task baseado na priridade dinamica dela
 task_t* scheduler(){
 	task_t *aux = FILA_PRONTOS;
 	task_t *atual = aux;
+	//percorre a listae encontra o elemento com menor prioridade
 	do{
 		if (aux->prio_din <= atual->prio_din)
 			atual = aux;
 		aux = aux->next;
 	}while(aux != FILA_PRONTOS);
 
-
+	//remove o elementoda lista para anao ser afetado pelo decremento de prioridade
 	queue_remove((queue_t**)&FILA_PRONTOS,(queue_t*)atual);
+	//retorna a prioridade para o original
 	atual->prio_din = atual->prio_est;
-	//printf("escolhido elemento com prioridade %i \n", atual->prio_din);
+	//decrementa a prioridade de todos os elementos da fila
 	if (queue_size((queue_t*)FILA_PRONTOS) > 0){
 		aux = FILA_PRONTOS;
 		do{
@@ -40,39 +49,48 @@ task_t* scheduler(){
 			aux = aux->next;
 		}while(aux != FILA_PRONTOS);
 	}	
+	//coloca o elemento selecionad de volta na fila de prontos
 	queue_append((queue_t **)&FILA_PRONTOS,(queue_t*)atual);
 
 	return atual;
 }
 
+//funcao de controle de fluxo, trava a volta da funcoes 
 void dispacher(){
 	//printf("inicio do dispatcher\n");
 	MAINAUX = DISPATCHER;
 	task_t *next = NULL;
 	while(queue_size((queue_t*)FILA_PRONTOS) > 0){
 		next = scheduler();
+		//reseta o code
 		CODE = PRONTA;
 		if (next != NULL){
+			//a volta do taskswitch modifica o CODE entao CODE muda entre 
+			//a atribuicao e agora
 			task_switch(next);
 
+			//tratamento de CODE 	
 			switch(CODE){
 				case TERMINADA:
 					queue_remove((queue_t**)&FILA_PRONTOS,(queue_t*)next);
 					//printf("tarefa terminada com sucesso\n");
 				break;
 				case RODANDO:
-					//printf("algo esta entranho a tarefa ainda esta roando\n");
+					//ain nao usado
+					//printf("algo esta estranho a tarefa ainda esta rodando\n");
 				break;
 				case DORMINDO:
-					//printf("tarefa dormindo\n");
+					//ainda nao usado
+					//printf("tarefa colocada para dormir\n");
 				break;
 				case PRONTA:
-					//printf("tarefa dormindo\n");
+					//ainda nao usado
+					//printf("tarefa pronta\n");
 				break;
 			}
 		}
 	}
-	//printf("fim do dispatcher\n");	
+	//recoloca a main para poder fazer um retorno	
 	MAINAUX = MAIN;
 	task_exit(0);
 }
@@ -83,14 +101,17 @@ void ppos_init(){
 	/* desativa o buffer da saida padrao (stdout), usado pela função printf */
 	setvbuf (stdout, 0, _IONBF, 0);
 
+	//cria a estrutura task de main
 	MAIN = malloc(sizeof(task_t));
 	MAIN->id = ID;
-	ID++;
+	
 	MAIN->status = 1;
 	MAIN->preemptable = 0;
 	getcontext(&MAIN->context);
 	task_setprio(MAIN,0);
 
+	//incrementa a ID e cria a primeira tarefa atual;
+	ID++;
 	ATUAL = MAIN;
 
 	DISPATCHER = malloc(sizeof(task_t));
@@ -103,46 +124,54 @@ void ppos_init(){
 // Cria uma nova tarefa. Retorna um ID> 0 ou erro.
 int task_create(task_t *task,void (*start_func)(void *),void *arg){
 
+	//cria o contexto
 	ucontext_t Context;
-	getcontext (&Context);
+	if (getcontext (&Context) < 0){
+		perror("erro na criacao de contexto:");
+		exit(1);
+	}	
 
+	//inicia as variaveis do contexto e aloca o buffer da stack
 	char *stack;
-   	stack = malloc (TAMBUFFER);
-   	if (stack){
-    	Context.uc_stack.ss_sp = stack;
-      	Context.uc_stack.ss_size = TAMBUFFER;
-      	Context.uc_stack.ss_flags = 0;
-      	Context.uc_link = 0;
-   	}
-   	else{
-      	perror ("Erro na criação da pilha: ") ;
-      	exit (1) ;
-   	}
+	stack = malloc (TAMBUFFER);
+	if (stack){
+	Context.uc_stack.ss_sp = stack;
+		Context.uc_stack.ss_size = TAMBUFFER;
+		Context.uc_stack.ss_flags = 0;
+		Context.uc_link = 0;
+	}
+	else{
+		perror("Erro na criação da pilha: ") ;
+		exit(1) ;
+	}
 
+	//inicia as variaveis do contexto
+	task->context = Context;
+	task->id = ID;
+	ID++;
+	task->status = PRONTA;
+	task->preemptable = 0;
+	task_setprio(task,0);
 
-   	task->context = Context;
-   	task->id = ID;
-   	ID++;
-   	task->status = 1;
-   	task->preemptable = 0;
-   	task_setprio(task,0);
+	makecontext(&task->context, (void*)(*start_func), 1, arg);
+	if (task != DISPATCHER){
+		queue_append((queue_t **)&FILA_PRONTOS,(queue_t*)task);
+	}
 
-   	makecontext (&task->context, (void*)(*start_func), 1, arg);
-   	if (task != DISPATCHER){
-   		queue_append((queue_t **)&FILA_PRONTOS,(queue_t*)task);
-   		//printf("tamanho: %i\n", queue_size((queue_t*)FILA_PRONTOS));
-   	}
-
-   	return 1;
+	return 1;
 }
 
 // Termina a tarefa corrente, indicando um valor de status encerramento
+// o retorno eh por padrao o dipsatacher mas pode ser setado usando MAINAUX (por exemplo como eh feito no proprio dispatcher)
 void task_exit(int exit_code) {
 	task_t *aux = ATUAL;
 	task_t *nova = MAINAUX;
 	ATUAL = MAINAUX;
 	CODE = exit_code;
-	swapcontext (&aux->context, &nova->context);
+	if (swapcontext (&aux->context, &nova->context) < 0){
+		perror("erro em troca de contexto: ");
+		exit(1);
+	}
 }
 
 // alterna a execução para a tarefa indicada
@@ -150,7 +179,10 @@ int task_switch(task_t *task){
 	task_t *aux = ATUAL;
 	task_t *nova = task;
 	ATUAL = task;
-	swapcontext (&aux->context, &nova->context);
+	if (swapcontext (&aux->context, &nova->context) < 0){
+		perror("erro em troca de contexto: ");
+		exit(1);
+	}
 	return 0;
 }
 
